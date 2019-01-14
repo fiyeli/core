93f94a8ae1e41eeede0e303247974de6ba9beccc
Thibault Ruby <thibault.ruby@outlook.fr>
2019-01-14 09:36:35 +0100

    move camera.py to camera repo

--- camera.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env  python
-from picamera import PiCamera
-import time
-
-camera = PiCamera()
-
-camera.start_preview()
-camera.capture("img/" + str(int(time.time())) + ".jpg")
-camera.stop_preview() 
